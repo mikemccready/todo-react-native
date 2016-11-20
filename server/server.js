8c3d37eb6f58918f3ec90c7d6113c7dbe9d42082
Mike McCready <mikem@inspero.net>
2016-11-20 14:29:06 -0800

    init express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
 // server.js
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + './../index.html'));
+})
+
+app.listen(PORT, () => {
+	console.log('listening port 3000..')
+})
